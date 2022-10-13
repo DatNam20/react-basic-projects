6675e768cfee65d3b6aaec75390f63509d18686e
DEEPALI SINGH <deepalisingh15899@gmail.com>
2022-10-13 23:18:24 +0530

    modified app_js

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,14 +1,17 @@
-
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 function App() {
+  const [people, setPeople] = useState(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Birthday Reminder app
-        </p>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
